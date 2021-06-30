c5718247585d9c2d7bcc2250aedf56ea0f811863
cristianakraemer <74330849+cristianakraemer@users.noreply.github.com>
2021-06-29 21:28:16 -0300

    CenarioBugado

--- PJD1-20211-Desafio-06/Assets/Scripts/EnemyController.cs
+++ PJD1-20211-Desafio-06/Assets/Scripts/EnemyController.cs
@@ -3,44 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum EnemyBehaviour { None, Patrol, Seek }
 public class EnemyController : Rigidbody2DBase, IPoolableObject
 {
     public int Hp { get; protected set; }
 
-    private NavMeshAgent map;
+    // NavMesh e seguir player
+    private NavMeshAgent agent;
+    private GameObject player;
 
-    // Seguir o jogador e campo de visão...
-    float SpeedEnemy = 2f;
-    float StopDistance = 3f;
-    private Transform TargetVision; // Campo de visão.
+    public EnemyBehaviour behaviour = EnemyBehaviour.Patrol;
+    [Header("Patrulha")]
+    public Transform current;
 
-    /*/ Patrulha
-    public Transform currentTarget; // Pivô, ponto zero.
-    public Transform point1; // Pivô 1.
-    public Transform point2; // Pivô 2. */
-
-    // Patrulha
-    public float distance;
-    bool isRight = true;
-    public Transform pointCheck;
 
     private void Start()
     {
         Hp = Random.Range(100, 201);
 
-        // Procura o GameObject com a Tag Player, acessando a sua posição.
-        TargetVision = GameObject.FindGameObjectWithTag("Player").transform;
-
-        //currentTarget = point1;
-
-        map = GetComponent<NavMeshAgent>();
-        map.updateRotation = false;
-        map.updateUpAxis = false;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        MoveEnemy();
+        Seek();
+
+        switch(behaviour)
+        {
+            case EnemyBehaviour.None:
+                break;
+            case EnemyBehaviour.Patrol:
+                break;
+            case EnemyBehaviour.Seek:
+                break;
+        }
     }
 
     public bool ApplyDamage(int damage)
@@ -62,45 +61,10 @@ public class EnemyController : Rigidbody2DBase, IPoolableObject
         Start();
     }
 
-    protected virtual void MoveEnemy()
+    protected virtual void Seek()
     {
-        // Só movimenta o inimigo se a distância entre os dois for menor que a escolhida no StopDistance. Basicamente um campo de visão.
-        if (Vector2.Distance(transform.position, TargetVision.position) < StopDistance)
-        {
-            // Movimenta o gameObject até um ponto específico com uma velocidade.
-            transform.position = Vector2.MoveTowards(transform.position, TargetVision.position, SpeedEnemy * Time.deltaTime);
-            Debug.LogFormat("Está vendo o Player");
-        }
-        else
-        {
-            transform.Translate(Vector2.right * SpeedEnemy * Time.deltaTime);
-            RaycastHit2D point = Physics2D.Raycast(pointCheck.position, Vector2.down, distance);
-
-            if (point.collider == false)
-            {
-                if (isRight == true)
-                {
-                    transform.eulerAngles = new Vector3(0, 0, 0);
-                    isRight = false;
-                }
-                else
-                {
-                    transform.eulerAngles = new Vector3(0, 180, 0);
-                    isRight = true;
-                }
-            }
-           
-            /*/
-            // movimentação do inimigo até os pivôs.
-            transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, SpeedEnemy * Time.deltaTime);
-
-            // movimentação do inimigo em patrulha, do pivô A ao pivô B, e vise versa.
-            if (currentTarget == point1 && transform.position == point1.position)
-                currentTarget = point2;
-            if (currentTarget == point2 && transform.position == point2.position)
-                currentTarget = point1;
-            */
-        }
+        agent.SetDestination(player.transform.position);
     }
+
 }
 
